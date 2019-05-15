bc15ba4ea9198c2340ce827ea52f6f138dd80e62
nuclearRPG <codingjs@163.com>
2019-05-15 21:46:54 +0000

    virutal console configs script improved

--- vconsole-kbd/vconsole-kbd.sh
+++ vconsole-kbd/vconsole-kbd.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 KEYMAP_DIR=/usr/local/share/kbd/keymaps
-mkdir -p KEYMAP_DIR
-cp personal.map $KEYMAP_DIR
-cp vconsole.conf /etc/
+mkdir -p $KEYMAP_DIR
+cp $(dirname $0)/personal.map $KEYMAP_DIR
+cp $(dirname $0)/vconsole.conf /etc/
